4ab132b25f74bcc44a0e23e6d96cb4d943e83b69
Deniz.Aksu <deniz.aksu@dijitalkurye.com.tr>
2021-11-29 00:41:25 +0300

    last commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -11,9 +11,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const CartItemRemoveHandler = (id) => {};
+  const CartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const CartItemAddHandler = (item) => {};
+  const CartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItem = cartCtx.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  const btnClass = `${classes.button} ${btnIsHighLighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    setBtnIsHighlighted(true);
+  }, []);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClass} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
